204e8eb86736e7929cdbef3a2590b85281312262
RadJKW <jwest@howard.com>
2022-12-16 17:32:08 -0600

    Almost have new file server & http client working in wasm.

--- MudBlazorPWA/Client/Program.cs
+++ MudBlazorPWA/Client/Program.cs
@@ -7,7 +7,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient("PwaServer", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+builder.Services.AddHttpClient("FileServer", client => client.BaseAddress = new Uri("http://localhost:3000"));
+
 builder.Services.AddMudServices();
+
 
 await builder.Build().RunAsync();
