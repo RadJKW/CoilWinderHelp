85175a637e6596d1b4db7bb6897d72fc1475158d
RadJKW <jwest@howard.com>
2022-12-30 16:36:59 -0600

    added windows and mac folder paths which are chosen by the compiler.

--- CoilWinderHelp.FileServer/Program.cs
+++ CoilWinderHelp.FileServer/Program.cs
@@ -14,17 +14,28 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 app.UseCors("AllowAll");
 
+var windowsPath = @"B:\CoilWinderTraining-Edit\";
+var macPath = @"/Users/jkw/WindingPractices/";
 app.UseFileServer(new FileServerOptions()
 {
+    
     EnableDirectoryBrowsing = true,
     RedirectToAppendTrailingSlash = true,
     DirectoryBrowserOptions =
     {
-        FileProvider = new PhysicalFileProvider(@"/Users/jkw/WindingPractices/")
+        /*FileProvider = new PhysicalFileProvider(@"/Users/jkw/WindingPractices/")
         {
             UseActivePolling = true,
             UsePollingFileWatcher = true
-        },
+        }*/
+        
+        // if the operating system is windows, use the windows path
+        // if the operating system is mac, use the mac path
+        FileProvider = new PhysicalFileProvider(OperatingSystem.IsWindows() ? windowsPath : macPath)
+        {
+            UseActivePolling = true,
+            UsePollingFileWatcher = true
+        }
     },
     
 });
