c06f50537e0ecf390c5a9bbc09960cc3761f2880
RadJKW <jwest@howard.com>
2023-01-03 16:58:55 -0600

    Created Reusable Components, Styling, New layout. Index page looks goood so far.

--- MudBlazorPWA/Client/Components/VideoPlayer.razor.js
+++ MudBlazorPWA/Client/Components/VideoPlayer.razor.js
@@ -3,69 +3,100 @@
 let video;
 let playButton;
 let pauseButton;
+let refreshButton;
+let seekForwardButton;
+let seekBackwardButton;
 let videoSeeked = false;
 
-export const init = () => {
-    console.log('initializing...')
-    video = document.getElementById('videoPlayer');
-    playButton = document.getElementById('playButton');
-    pauseButton = document.getElementById('pauseButton');
+export const init = (videoPlayerId, buttonIds) => {
+	console.log('initializing...')
+	video = document.getElementById(videoPlayerId);
+	playButton = document.getElementById(buttonIds[0]);
+	pauseButton = document.getElementById(buttonIds[1]);
+	refreshButton = document.getElementById(buttonIds[2]);
+	seekForwardButton = document.getElementById(buttonIds[3]);
+	seekBackwardButton = document.getElementById(buttonIds[4]);
 
-    video.volume = 0.1;
-    video.addEventListener('seeked', () => {
-            videoSeeked = true;
-            console.log('seeked');
-        }
-    );
-    video.addEventListener('play', () => {
-            if (pauseButton != null) {
-                pauseButton.disabled = false;
-            }
-            if (playButton != null) {
-                playButton.disabled = true;
-            }
-        }
-    );
-    video.addEventListener('pause', () => {
-            if (pauseButton != null) {
-                pauseButton.disabled = true;
-            }
-            if (playButton != null) {
-                playButton.disabled = false;
-            }
-        }
-    );
+	console.log(video.id);
+	console.log(playButton.id);
+	console.log(pauseButton.id);
+	console.log(refreshButton.id);
+	console.log(seekForwardButton.id);
+	console.log(seekBackwardButton.id);
+
+
+	video.volume = 0.1;
+	video.addEventListener('seeked', () => {
+			videoSeeked = true;
+			console.log('seeked');
+		}
+	);
+	video.addEventListener('play', () => {
+			if (pauseButton != null) {
+				pauseButton.disabled = false;
+			}
+			if (playButton != null) {
+				playButton.disabled = true;
+			}
+			if (seekBackwardButton != null) {
+				seekBackwardButton.disabled = false;
+			}
+			if (seekForwardButton != null) {
+				seekForwardButton.disabled = false;
+			}
+		}
+	);
+	video.addEventListener('pause', () => {
+			if (pauseButton != null) {
+				pauseButton.disabled = true;
+			}
+			if (playButton != null) {
+				playButton.disabled = false;
+			}
+			if (seekBackwardButton != null) {
+				seekBackwardButton.disabled = true;
+			}
+			if (seekForwardButton != null) {
+				seekForwardButton.disabled = true;
+			}
+		}
+	);
+
+	playButton.disabled=false;
+	pauseButton.disabled=true;
+	refreshButton.disabled=false;
+
 };
 
 export const pauseVideo = () => {
-    video.pause();
-    pauseButton.disabled = true;
-    playButton.disabled = false;
+	video.pause();
+	pauseButton.disabled = true;
+	playButton.disabled = false;
 
 };
 export const restartVideo = () => {
-    video.pause();
-    video.load();
-    video.currentTime = 0;
-    video.play();
+	video.pause();
+	video.load();
+	video.currentTime = 0;
+	video.play();
 };
 export const playVideo = () => {
-    video.play();
-    playButton.disabled = true;
-    pauseButton.disabled = false
+	video.play();
+	playButton.disabled = true;
+	pauseButton.disabled = false
 };
 export const seekVideo = (time, direction) => {
-    // if mathtype is 0 then subtract time from current time
-    // if mathtype is 1 then add time to current time
-    if (direction === false) {
-        video.currentTime -= time;
-    }
-    if (direction === true) {
-        video.currentTime += time;
-    }
+	// if mathtype is 0 then subtract time from current time
+	// if mathtype is 1 then add time to current time
+	if (direction === false) {
+		video.currentTime -= time;
+	}
+	if (direction === true) {
+		video.currentTime += time;
+	}
 
 
 }
 
-// return the duration of the video in seconds 
+// return the duration of the video in seconds
 export const getVideoDuration = () => parseInt(video.duration);
--- MudBlazorPWA/Client/Pages/Index.razor.js
+++ MudBlazorPWA/Client/Pages/Index.razor.js
@@ -1,70 +1,70 @@
-﻿// noinspection JSUnusedGlobalSymbols
-
-let video;
-let playButton;
-let pauseButton;
-let videoSeeked = false;
-
-export const init = () => {
-    video = document.getElementById('videoPlayer');
-    playButton = document.getElementById('playButton');
-    pauseButton = document.getElementById('pauseButton');
-    
-    video.volume = 0.1;
-    video.addEventListener('seeked', () => {
-            videoSeeked = true;
-            console.log('seeked');
-        }
-    );
-    video.addEventListener('play', () => {
-            if (pauseButton != null) {
-                pauseButton.disabled = false;
-            }
-            if (playButton != null) {
-                playButton.disabled = true;
-            }
-        }
-    );
-    video.addEventListener('pause', () => {
-            if (pauseButton != null) {
-                pauseButton.disabled = true;
-            }
-            if (playButton != null) {
-                playButton.disabled = false;
-            }
-        }
-    );
-};
-
-export const pauseVideo = () => {
-    video.pause();
-    pauseButton.disabled = true;
-    playButton.disabled = false;
-
-};
-export const restartVideo = () => {
-    video.pause();
-    video.load();
-    video.currentTime = 0;
-    video.play();
-};
-export const playVideo = () => {
-    video.play();
-    playButton.disabled = true;
-    pauseButton.disabled = false
-};
-export const seekVideo = (time, direction) => {
-    // if mathtype is 0 then subtract time from current time
-    // if mathtype is 1 then add time to current time
-    if (direction === false) {
-        video.currentTime -= time;
-    }
-    if (direction === true) {
-        video.currentTime += time;
-    }
-
-
-}
-
-// return the duration of the video in seconds 
-export const getVideoDuration = () => parseInt(video.duration);
+﻿// // noinspection JSUnusedGlobalSymbols
+//
+// let video;
+// let playButton;
+// let pauseButton;
+// let videoSeeked = false;
+//
+// export const init = () => {
+//     video = document.getElementById('videoPlayer');
+//     playButton = document.getElementById('playButton');
+//     pauseButton = document.getElementById('pauseButton');
+//    
+//     video.volume = 0.1;
+//     video.addEventListener('seeked', () => {
+//             videoSeeked = true;
+//             console.log('seeked');
+//         }
+//     );
+//     video.addEventListener('play', () => {
+//             if (pauseButton != null) {
+//                 pauseButton.disabled = false;
+//             }
+//             if (playButton != null) {
+//                 playButton.disabled = true;
+//             }
+//         }
+//     );
+//     video.addEventListener('pause', () => {
+//             if (pauseButton != null) {
+//                 pauseButton.disabled = true;
+//             }
+//             if (playButton != null) {
+//                 playButton.disabled = false;
+//             }
+//         }
+//     );
+// };
+//
+// export const pauseVideo = () => {
+//     video.pause();
+//     pauseButton.disabled = true;
+//     playButton.disabled = false;
+//
+// };
+// export const restartVideo = () => {
+//     video.pause();
+//     video.load();
+//     video.currentTime = 0;
+//     video.play();
+// };
+// export const playVideo = () => {
+//     video.play();
+//     playButton.disabled = true;
+//     pauseButton.disabled = false
+// };
+// export const seekVideo = (time, direction) => {
+//     // if mathtype is 0 then subtract time from current time
+//     // if mathtype is 1 then add time to current time
+//     if (direction === false) {
+//         video.currentTime -= time;
+//     }
+//     if (direction === true) {
+//         video.currentTime += time;
+//     }
+//
+//
+// }
+//
+// // return the duration of the video in seconds 
+// export const getVideoDuration = () => parseInt(video.duration);
