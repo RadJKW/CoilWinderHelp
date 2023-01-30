a246d87cfab0a1de88ca7c696cc6dca8ec8ac122
RadJKW <jwest@howard.com>
2023-01-30 17:59:51 -0600

    Added functionality for Unix System (Mac)

--- MudBlazorPWA/Shared/Services/DocViewService.cs
+++ MudBlazorPWA/Shared/Services/DocViewService.cs
@@ -1,24 +1,21 @@
 ﻿namespace MudBlazorPWA.Shared.Services;
 public class DocViewService
 {
-    public event EventHandler? MajorUpdateOccured;
-    private void OnMajorUpdateOccured() => MajorUpdateOccured?.Invoke(this, EventArgs.Empty);
+	public event EventHandler? MajorUpdateOccured;
+	private void OnMajorUpdateOccured() => MajorUpdateOccured?.Invoke(this, EventArgs.Empty);
 
-    public AppSettings AppSettings {get ; private set; } = new();
+	public AppSettings AppSettings { get; private set; } = new();
 
-    public void SetAppSettings (AppSettings appSettings)
-    {
-        AppSettings = appSettings;
-        OnMajorUpdateOccured();
-    }
-
-
+	public void SetAppSettings(AppSettings appSettings) {
+		AppSettings = appSettings;
+		OnMajorUpdateOccured();
+	}
 }
 
 public class AppSettings
 {
-    public string RootDirectoryPath { get; set; } = string.Empty;
-    public bool AppBarIsVisible { get; set; } = true;
-    public bool DrawerOpen { get; set; } = true;
-    public bool DrawerOpenOnHover { get; set; } = false;
+	public string RootDirectoryPath { get; set; } = string.Empty;
+	public bool AppBarIsVisible { get; set; } = true;
+	public bool DrawerOpen { get; set; } = true;
+	public bool DrawerOpenOnHover { get; set; } = false;
 }
--- MudBlazorPWA/Shared/Services/LayoutService.cs
+++ MudBlazorPWA/Shared/Services/LayoutService.cs
@@ -3,29 +3,25 @@ using MudBlazor;
 namespace MudBlazorPWA.Shared.Services;
 public class LayoutService
 {
-  public bool IsDarkMode { get; private set; }
+	public bool IsDarkMode { get; private set; }
 
-  public MudTheme? CurrentTheme { get; private set; }
+	public MudTheme? CurrentTheme { get; private set; }
 
-  public void SetDarkMode(bool value)
-  {
-    IsDarkMode = value;
-  }
+	public void SetDarkMode(bool value) {
+		IsDarkMode = value;
+	}
 
-  public event EventHandler MajorUpdateOccured = null!;
+	public event EventHandler MajorUpdateOccured = null!;
 
-  private void OnMajorUpdateOccured() => MajorUpdateOccured.Invoke(this, EventArgs.Empty);
+	private void OnMajorUpdateOccured() => MajorUpdateOccured.Invoke(this, EventArgs.Empty);
 
-  public void ToggleDarkMode()
-  {
-    IsDarkMode = !IsDarkMode;
-    OnMajorUpdateOccured();
-  }
+	public void ToggleDarkMode() {
+		IsDarkMode = !IsDarkMode;
+		OnMajorUpdateOccured();
+	}
 
-  public void SetBaseTheme(MudTheme theme)
-  {
-    CurrentTheme = theme;
-    OnMajorUpdateOccured();
-  }
-
+	public void SetBaseTheme(MudTheme theme) {
+		CurrentTheme = theme;
+		OnMajorUpdateOccured();
+	}
 }
