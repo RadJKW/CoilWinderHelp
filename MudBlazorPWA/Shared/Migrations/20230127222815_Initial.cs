65b4beffb4c93f807918399e8d4b00b283e9797b
RadJKW <jwest@howard.com>
2023-01-30 08:59:54 -0600

    Added Migrations to VCS and upgraded projects.

---
+++ MudBlazorPWA/Shared/Migrations/20230127222815_Initial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MudBlazorPWA.Shared.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WindingCodes",
+                columns: table => new
+                {
+                    Code = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    FolderPath = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WindingCodes", x => x.Code);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WindingCodes");
+        }
+    }
+}
