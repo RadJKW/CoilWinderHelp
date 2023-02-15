48199fc64d7f0ff3eea8830e27a4de9f527d03f8
RadJKW <jwest@howard.com>
2023-02-15 15:25:33 -0600

    Added Crud to Hub after figuring out how to directly return data when method invoked.

--- MudBlazorPWA/Shared/Hubs/DirectoryHub.cs
+++ MudBlazorPWA/Shared/Hubs/DirectoryHub.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.AspNetCore.Http.Features;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using MudBlazorPWA.Shared.Data;
 using MudBlazorPWA.Shared.Models;
 using MudBlazorPWA.Shared.Services;
 
@@ -18,10 +20,12 @@ public class DirectoryHub : Hub<IHubClient>
     #region Constructor
     private readonly IDirectoryService _directoryService;
     private readonly ILogger<DirectoryHub> _logger;
-    public DirectoryHub(IDirectoryService directoryService, ILogger<DirectoryHub> logger)
+    private readonly IDataContext _dataContext;
+    public DirectoryHub(IDirectoryService directoryService, ILogger<DirectoryHub> logger, IDataContext dataContext)
     {
         _directoryService = directoryService;
         _logger = logger;
+        _dataContext = dataContext;
     }
     #endregion
 
@@ -76,11 +80,84 @@ public class DirectoryHub : Hub<IHubClient>
         await Clients.Group(clientIp).ReceiveAllFolders(folders);
     }
 
-    public async Task SaveWindingCodesDb(IEnumerable<WindingCode> windingCodes, bool syncDatabase)
+    public async Task<string> SaveWindingCodesDb(IEnumerable<WindingCode> windingCodes, bool syncDatabase)
     {
         var clientIp = GetConnectionIp(Context);
         await _directoryService.ExportWindingCodesToJson(windingCodes, syncDatabase);
         await Clients.Group(clientIp).WindingCodesDbUpdated();
+        return "From server: WindingCodes.json saved.";
+    }
+    #endregion
+
+    #region DataBase CRUD
+    public async Task<IEnumerable<WindingCode>?> GetWindingCodes()
+    {
+        // return the codes as a list if any exist
+        var windingCodes = await _dataContext.WindingCodes.ToListAsync();
+        return windingCodes.Any() ? windingCodes : null;
+    }
+
+    public async Task<WindingCode?> GetWindingCode(string codeName)
+    {
+        var windingCode = await _dataContext.WindingCodes.FirstOrDefaultAsync( e => e.Name == codeName);
+
+        return windingCode ?? null;
+    }
+
+    public async Task<WindingCode?> UpdateWindingCode(string codeName, WindingCode windingCode)
+    {
+        if (codeName != windingCode.Name)
+        {
+            return null;
+        }
+        ((DataContext)_dataContext).Entry(windingCode).State = EntityState.Modified;
+        try
+        {
+            await _dataContext.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!WindingCodeExists(codeName))
+            {
+                return null;
+            }
+            else
+            {
+                throw;
+            }
+        }
+        return windingCode;
+    }
+
+    /*public async Task PutWindingCode(string codeName, WindingCode windingCode)
+    {
+    }*/
+
+    public async Task<bool> CreateWindingCode(WindingCode windingCode) {
+
+        if (WindingCodeExists(windingCode.Name!))
+        {
+            return false;
+        }
+        _dataContext.WindingCodes.Add(windingCode);
+        await _dataContext.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task DeleteWindingCode(string codeName)
+    {
+        var windingCode = await _dataContext.WindingCodes.FirstOrDefaultAsync( e => e.Name == codeName);
+        if (windingCode is null)
+        {
+            return;
+        }
+        _dataContext.WindingCodes.Remove(windingCode);
+        await _dataContext.SaveChangesAsync();
+    }
+
+    private bool WindingCodeExists(string codeName)
+    {
+        return _dataContext.WindingCodes.Any( e => e.Name == codeName);
     }
     #endregion
 
