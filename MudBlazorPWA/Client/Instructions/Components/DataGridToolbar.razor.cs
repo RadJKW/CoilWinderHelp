f2234179f09239896385b3830173c5939eef4b71
RadJKW <jwest@howard.com>
2023-03-16 16:28:38 -0500

    Fixed and Added StopEmulator.razor page back to NavMenu to use as template for GUI StopEditor;

---
+++ MudBlazorPWA/Client/Instructions/Components/DataGridToolbar.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazorPWA.Shared.Models;
+
+namespace MudBlazorPWA.Client.Instructions.Components;
+public partial class DataGridToolbar : ComponentBase
+{
+	[Parameter] public bool EnableEdit { get; set; }
+	[Parameter] public string? SearchString { get; set; }
+	[Parameter] public List<Division> SelectedDivisions { get; set; } = new List<Division>();
+	[Parameter] public EventCallback<(bool, Division)> OnDivisionToggled { get; set; }
+
+	[Parameter] public EventCallback<bool> EnableEditChanged { get; set; }
+
+	private async Task OnDivisionCheckedChanged(bool isChecked, Division division) {
+		await OnDivisionToggled.InvokeAsync((isChecked, division));
+	}
+
+	protected override async Task OnInitializedAsync() {
+		 await base.OnInitializedAsync();
+
+		 if (EnableEditChanged.HasDelegate)
+			 _ = EnableEditChanged.InvokeAsync(EnableEdit);
+	}
+}
