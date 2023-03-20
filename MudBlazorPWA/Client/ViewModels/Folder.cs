e3ae05c47c121ce7666d5fd6f9de7eaa17c8d8b3
RadJKW <jwest@howard.com>
2023-03-20 16:08:14 -0500

    WIP -> FolderSelector.razor now can display if the folder has Media (PDFs & Videos).

---
+++ MudBlazorPWA/Client/ViewModels/Folder.cs
@@ -0,0 +1,23 @@
+namespace MudBlazorPWA.Client.ViewModels;
+public class Folder {
+	public Folder(string name, string? path, char splitChar) {
+		Name = name;
+		Path = path;
+		SplitChar = splitChar;
+	}
+	public string Name { get; }
+	public string? Path { get; }
+	public bool IsDisabled { get; set; }
+	public MediaFiles MediaFiles { get; } = new();
+	public Folder? RefMediaFiles { get; set; } = null;
+	public List<Folder> SubFolders { get; } = new();
+
+	public List<DropItem> DropItems { get; } = new();
+	public char SplitChar { get; }
+}
+
+public class MediaFiles
+{
+	public List<string>? PdfFiles { get; set; }
+	public List<string>? VideoFiles { get; set; }
+}
