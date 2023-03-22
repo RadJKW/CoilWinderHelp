e7f02a58f86e68365db977378826843c2cd13ec9
RadJKW <jwest@howard.com>
2023-03-22 16:49:43 -0500

    Finished styling FolderSelector.Razor; WIP -> Style DataGrid drop-zones, Add dbSaveChanges.

--- MudBlazorPWA/Client/wwwroot/js/site.js
+++ MudBlazorPWA/Client/wwwroot/js/site.js
@@ -1,6 +1,19 @@
-﻿/* eslint-disable no-unused-vars */
+/* eslint-disable no-unused-vars */
 // noinspection JSUnusedGlobalSymbols
 
 function openFilePreview (url) {
   window.open(url, '_blank')
 }
+
+function checkOverflowingElements () {
+  const selector = '#folder-selector .drop-zone-item > * p'
+  const elements = document.querySelectorAll(selector)
+
+  elements.forEach((element) => {
+    if (element.offsetWidth < element.scrollWidth) {
+      element.parentElement.parentElement.classList.add('overflowing')
+    }
+  })
+}
+
+// Run the function on page load
