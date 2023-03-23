3cd24e54f0c94aa3c1c8951e5b19cc06ce104fbe
RadJKW <jwest@howard.com>
2023-03-23 16:44:12 -0500

    DropItems are now copied from zone-to-zone. BUG -> Item added to end of DropZone when copied;

--- MudBlazorPWA/Client/ViewModels/DropItem.cs
+++ MudBlazorPWA/Client/ViewModels/DropItem.cs
@@ -1,10 +1,16 @@
 ﻿namespace MudBlazorPWA.Client.ViewModels;
 public class DropItem
 {
+
+	public string? OriginalIdentifier { get; set; }
 	public string? Identifier { get; set; }
 	public string? Name { get; set; }
 	public string? Path { get; set; }
 	public DropItemType Type { get; set; }
+
+	public bool IsDisabled { get; set; }
+
+	public bool IsCopy { get; set; }
 }
 
 public enum DropItemType
