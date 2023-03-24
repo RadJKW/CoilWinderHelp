f8aaf9b5e6795fb1c616904d57d39f46df85de31
RadJKW <jwest@howard.com>
2023-03-24 15:57:11 -0500

    Added tooltip for showing locations where dropItem is copied to. Removed ItemRenderer from dropZones in FolderSelector.razor to DropContainer.ItemRenderer

--- MudBlazorPWA/Client/wwwroot/js/site.js
+++ MudBlazorPWA/Client/wwwroot/js/site.js
@@ -1,11 +1,12 @@
-/* eslint-disable no-unused-vars */
 // noinspection JSUnusedGlobalSymbols
+/* eslint-disable no-unused-vars */
 
 function openFilePreview (url) {
   window.open(url, '_blank')
 }
 
 function checkOverflowingElements () {
+  console.log('checkOverflowingElements')
   const selector = '#folder-selector .drop-zone-item > * p'
   const elements = document.querySelectorAll(selector)
 
@@ -13,6 +14,51 @@ function checkOverflowingElements () {
     if (element.offsetWidth < element.scrollWidth) {
       element.parentElement.parentElement.classList.add('overflowing')
     }
+  })
+  addAttributeToElements()
+}
+
+function addAttributeToElements () {
+  const selector = '#folder-selector .drop-zone-item'
+  const elements = document.querySelectorAll(selector)
+
+  elements.forEach((element) => {
+    // if the element does not have a [data-title] attribute,
+    // or if the data-title doesnt have any text, dont transfer it to the parent
+    const tooltipText = element.attributes['data-title']?.value
+    if (!tooltipText) return
+    AddTooltipEventListeners(element, tooltipText)
+    // add the attribute to the parent
+  })
+}
+
+function createCustomTooltip () {
+  const tooltip = document.createElement('div')
+  // get the first parent of the element that has a class of 'drop-zone-gallery'
+  tooltip.classList.add('custom-tooltip')
+  // Append the tooltip to the body instead of the chip element to avoid being constrained by the chip's container
+  document.body.appendChild(tooltip)
+}
+function AddTooltipEventListeners (element, tooltipText) {
+  const tooltip = document.querySelector('.custom-tooltip')
+  tooltip.innerHTML = tooltipText
+  element.addEventListener('mouseenter', () => {
+    const elementRect = element.getBoundingClientRect()
+    const elementRight = elementRect.right
+    const elementLeft = elementRect.left
+    const elementBottom = elementRect.bottom
+    const elementTop = elementRect.top
+
+    tooltip.innerHTML = tooltipText
+    tooltip.style.left = `${elementLeft}px`
+    tooltip.style.right = `${elementRight}px`
+    tooltip.style.top = `${elementTop + element.offsetHeight}px` // Adjust the value as needed for vertical positioning
+    tooltip.style.visibility = 'visible'
+    tooltip.style.maxWidth = `${element.offsetWidth}px`
+  })
+
+  element.addEventListener('mouseleave', () => {
+    tooltip.style.visibility = 'hidden'
   })
 }
 
