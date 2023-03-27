903e398acb3288242e59c9898fa8f5f1cd5a5fd5
RadJKW <jwest@howard.com>
2023-03-27 16:36:29 -0500

    Drop-Items in WindingCodeMediaDropZones.razor can now save to DataBase. FolderSelector.razor is parsing all folders and media files to drop items. WIP -> create multiple dropItems for each (folder, file) if need to appear in multiple drop-zones.

---
+++ MudBlazorPWA/Client/Services/FolderService.cs
@@ -0,0 +1,22 @@
+﻿using MudBlazorPWA.Client.ViewModels;
+
+namespace MudBlazorPWA.Client.Services;
+
+// functionality for this folder service
+// Properties:
+// - get the root folder ( the folder that contains the CoilWinderHelp Docs)
+// - keep track of the current folder
+// - keep track of the breadcrumbs ( history of folders the user has navigated to )
+// - provide the subfolders (used to show the user the folders they can navigate to)
+// Events:
+// - when the current folder changes, the subfolders and breadcrumbs should be updated
+//
+
+public class FolderService
+{
+	public Folder RootFolder { get; set; } = default!;
+	public Folder CurrentFolder { get; set; } = default!;
+	public List<Folder> Breadcrumbs { get; set; } = new();
+	public List<Folder> SubFolders { get; set; } = new();
+
+}
