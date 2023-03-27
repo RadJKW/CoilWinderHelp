b1a4af257f683f58b2c01acb854bb6c20bdd7676
RadJKW <jwest@howard.com>
2023-03-27 09:16:42 -0500

    AppSettings now has constructor where Drawers opens on hover by default.

--- MudBlazorPWA/Shared/Models/AppSettings.cs
+++ MudBlazorPWA/Shared/Models/AppSettings.cs
@@ -1,9 +1,16 @@
 namespace MudBlazorPWA.Shared.Models;
 public class AppSettings
 {
+
+	public AppSettings(bool? drawerOpen = null, bool? appBarIsVisible = null, bool? drawerOpenOnHover = null)
+	{
+		DrawerOpen = drawerOpen ?? DrawerOpen;
+		AppBarIsVisible = appBarIsVisible ?? AppBarIsVisible;
+		DrawerOpenOnHover = drawerOpenOnHover ?? DrawerOpenOnHover;
+	}
 	public bool DrawerOpen { get; set; }
 	public bool AppBarIsVisible { get; init; } = true;
-	public bool DrawerOpenOnHover { get; init; }
+	public bool DrawerOpenOnHover { get; init; } = true;
 
 
 
